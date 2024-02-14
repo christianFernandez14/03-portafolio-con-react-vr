abdbff43bcbdfa72c6bb514378a469388b693b9e
Christian Fernandez <christian14@gmail.com>
2024-02-14 16:23:19 -0300

    Trabajando con la barra de navegación, para trabajar con sus rutas / .107

--- src/components/layout/HeaderNav.jsx
+++ src/components/layout/HeaderNav.jsx
@@ -1,15 +1,28 @@
 
+import { NavLink } from "react-router-dom"
+
 const HeaderNav = () => {
   return (
     <header>
       <h1>Nombre usuario Portafolio</h1>
       <nav>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <NavLink to='inicio'>Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to='portafolio'>Portafolio</NavLink>
+          </li>
+          <li>
+            <NavLink to='servicios'>Servicios</NavLink>
+          </li>
+          <li>
+            <NavLink to='curriculum'>Curriculum</NavLink>
+          </li>
+          <li>
+            <NavLink to='contacto'>Contacto</NavLink>
+          </li>
+
         </ul>
       </nav>
     </header>
